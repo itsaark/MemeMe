e2032e5c093fd3a54e7c906874e34e7ba031656c
Arjun <itsarjunn@gmail.com>
2016-09-14 01:40:24 +0530

    Add default Text Attributes and Image Picker controller source

---
+++ MemeMe/MemeMe/MemeMe/TextAttributes.swift
@@ -0,0 +1,17 @@
+//
+//  TextAttributes.swift
+//  MemeMe
+//
+//  Created by Arjun Kodur on 9/14/16.
+//  Copyright © 2016 Arjun Kodur. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let memeTextAttributes = [
+    NSStrokeColorAttributeName : UIColor.blackColor(),
+    NSForegroundColorAttributeName : UIColor.whiteColor(),
+    NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+    NSStrokeWidthAttributeName : "-2.0"
+]
--- MemeMe/MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/MemeMe/ViewController.swift
@@ -8,27 +8,106 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
     
     @IBOutlet weak var imagePickerView: UIImageView!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    @IBOutlet weak var topText: UITextField!
+    
+    @IBOutlet weak var bottomText: UITextField!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        topText.delegate = self
+        bottomText.delegate = self
+        
+        topText.defaultTextAttributes = memeTextAttributes
+        bottomText.defaultTextAttributes = memeTextAttributes
+        
+        topText.text = "TOP"
+        bottomText.text = "BOTTOM"
+        
+        topText.textAlignment = .Center
+        bottomText.textAlignment = .Center
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func pickAnImage(sender: AnyObject) {
+    
+    override func viewWillAppear(animated: Bool) {
         
-        let imagePickerController = UIImagePickerController()
-        self.presentViewController(imagePickerController, animated: true, completion: nil)
-        
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
 
+    @IBAction func pickAnImageFromLibrary(sender: AnyObject) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func pickAnImageFromCamera(sender: AnyObject) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if let image = info["UIImagePickerControllerOriginalImage"]  as? UIImage {
+            
+            imagePickerView.image = image
+            
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+        if textField ==  topText &&  topText.text == "TOP" {
+            
+            topText.text = ""
+            
+        }else if textField == bottomText && bottomText.text == "BOTTOM" {
+            
+            bottomText.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if textField == topText {
+            
+            topText.resignFirstResponder()
+            
+        } else if textField == bottomText{
+            
+            bottomText.resignFirstResponder()
+        }
+        
+        return true
+    }
 }
 
